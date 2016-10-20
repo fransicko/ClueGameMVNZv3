200976f184a027b8a38f0a7d3313aa36bdfa1134
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-10-20 11:12:10 -0600

    Added the new class to our git

---
+++ src/clueGame/Player.java
@@ -0,0 +1,21 @@
+package clueGame;
+
+import java.awt.Color;
+
+public class Player {
+	private String Name;
+	private int row;
+	private int column;
+	private Color color;
+	
+	public Player(String name, int row, int column, Color color) {
+		super();
+		Name = name;
+		this.row = row;
+		this.column = column;
+		this.color = color;
+	}
+	
+	
+
+}
