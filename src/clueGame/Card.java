329bb717bec3a80b528311bb5497d9f62ab751e9
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-10-20 12:15:37 -0600

    saved

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -4,6 +4,12 @@ public class Card {
 	private String cardName;
 	private CardType type;
 	
+	public Card(String cardName, CardType type) {
+		super();
+		this.cardName = cardName;
+		this.type = type;
+	}
+
 	public boolean equals() {
 		// Made it false because it wont let it be null
 		return false;
