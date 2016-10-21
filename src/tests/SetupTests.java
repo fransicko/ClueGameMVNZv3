9968f2a6239089810667b26695c1e5cc548c64b1
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-10-20 18:19:01 -0600

    I changed the set config by adding two more strings

--- src/tests/SetupTests.java
+++ src/tests/SetupTests.java
@@ -2,10 +2,31 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class SetupTests {
+import clueGame.Board;
 
+public class SetupTests {
+	// Constants that I will use to test whether the file was loaded correctly
+	public static final int LEGEND_SIZE = 11;
+	public static final int NUM_ROWS = 25;
+	public static final int NUM_COLUMNS = 26;
+
+	// NOTE: I made Board static because I only want to set it up one 
+	// time (using @BeforeClass), no need to do setup before each test.
+	private static Board board;
+
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueBoard.csv", "ClueLegend.txt", "Players.txt", "weapons.txt");		
+		// Initialize will load BOTH config files 
+		board.initialize();
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
