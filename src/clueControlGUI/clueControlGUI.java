7e686eb4cd8d6c007ef4a5abe0b0010a4a82f5f6
The Ngo <The@laathe.mines.edu>
2016-10-27 11:39:51 -0600

    Added buttons panel

--- src/clueControlGUI/clueControlGUI.java
+++ src/clueControlGUI/clueControlGUI.java
@@ -1,17 +1,50 @@
 package clueControlGUI;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class clueControlGUI extends JFrame {
+
+public class clueControlGUI extends JPanel{
+	private JTextField name;
+	
 	public clueControlGUI() {
-		setSize(new Dimension(800,400));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout(2,3));
+		JPanel panel = createNamePanel();
+		add(panel);
+		panel = createButtonPanel();
+		add(panel);
+		
+		
+	}
+	
+	private JPanel createNamePanel() {
+		
+	}
+	
+	private JPanel createButtonPanel() {
+		JButton nextPlayer = new JButton("Next player");
+		JButton makeAccusation = new JButton("Make an accusation");
+		JPanel panel = new JPanel();
+		panel.add(nextPlayer);
+		panel.add(makeAccusation);
+		return panel;
 	}
 	
 	public static void main(String[] args) {
-		clueControlGUI frame = new clueControlGUI();
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(new Dimension(800,400));
 		frame.setVisible(true);
+		
+		clueControlGUI control = new clueControlGUI();
+		frame.add(control, BorderLayout.CENTER);
+		
+		
 	}
 }
